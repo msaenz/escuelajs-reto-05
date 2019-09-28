aa97a72766fd92a79ddb947a4041032c3b6e6119
Manuel Melgarejo Saenz <manuelmelgarejo@gmail.com>
2019-09-28 15:32:18 -0500

    Solucion Problema 4

---
+++ src/index_sol3.js
@@ -0,0 +1,57 @@
+const $app = document.getElementById('app');
+const $observe = document.getElementById('observe');
+const  API = 'https://rickandmortyapi.com/api/character/';
+//let  miStorage = window.localStorage;
+//miStorage.setItem('next_fetch',API)
+sessionStorage.setItem('next_fetch',API);
+
+const getData = api => {
+  fetch(api)
+    .then(response => response.json())
+    .then(response => {
+      apis = response
+      console.log(apis)
+      if (response.info.next === "") {
+        //miStorage.setItem('next_fetch',API)
+        alert("Ya no hay más personajes")
+        sessionStorage.setItem('next_fetch',API);
+      } else{
+        sessionStorage.setItem('next_fetch',response.info.next);
+        //miStorage.setItem('next_fetch',response.info.next)  
+        //miStorage.setItem('pages',response.info.pages)
+      }
+      const characters = response.results;
+      let output = characters.map(character => {
+        return `
+      <article class="Card">
+        <img src="${character.image}" />
+        <h2>${character.name}<span>${character.species}</span></h2>
+      </article>
+    `
+      }).join('');
+      let newItem = document.createElement('section');
+      newItem.classList.add('Items');
+      newItem.innerHTML = output;
+      $app.appendChild(newItem);
+    })
+    .catch(error => console.log(error));
+}
+
+async function loadData()  {
+  try {
+    //getData(miStorage.getItem('next_fetch'));    
+    await getData(sessionStorage.getItem('next_fetch'));
+  } catch (error) {
+    console.log(`Error en la API: ${error}`)
+  }
+}
+
+const intersectionObserver = new IntersectionObserver(entries => {
+  if (entries[0].isIntersecting) {
+    loadData();
+  }
+}, {
+  rootMargin: '0px 0px 100% 0px',
+});
+
+intersectionObserver.observe($observe);
